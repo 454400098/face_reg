a49018625bcf7d11c0c2d927b68f791d20f88ffd
Haodong Zheng <a454400098@icloud.com>
2019-02-02 21:27:44 -0500

    finish oninput change

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,9 @@
 import React from 'react';//standard import
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+// destructuring
+
+const ImageLinkForm = ({onInputChange}) => {
   return (
     <div>
       <p className ='f3'>
@@ -9,7 +11,7 @@ const ImageLinkForm = () => {
       </p>
       <div  className ='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex'/>
+          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
           <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'> Detect</button>
         </div>
        </div>
