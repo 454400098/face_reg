bccbf06968713582c0a02b43892dc945ca5896f0
Haodong Zheng <a454400098@icloud.com>
2019-02-02 21:17:18 -0500

    Front - end work in progress

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';//standard import
+
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='white f3'>
+      {'Haodong, Your Current rank is'}
+      </div>
+    <div className='white f1'>
+      {'#5'}
+      </div>
+    </div>
+  );
+}
+
+
+export default Rank;
