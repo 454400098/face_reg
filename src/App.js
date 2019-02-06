8d8bf35cea866f1a8ef9a55290703d79d0f07c15
Haodong Zheng <a454400098@icloud.com>
2019-02-05 21:35:09 -0500

    minor bug fixed

--- src/App.js
+++ src/App.js
@@ -28,24 +28,25 @@ const particlesOptions= {
     }
 }
 
+const initialState = {
+  input: ' ',
+  imageUrl:' ',
+  box:{},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name:'',
+    email: '',
+    entries:0,
+    joined:''
+  }
+}
 class App extends Component {
   constructor(){
     super();
-    this.state = {
-      input: ' ',
-      imageUrl:' ',
-      box:{},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name:'',
-        email: '',
-        entries:0,
-        joined:''
-      }
+    this.state = initialState;
     }
-  }
 
   loadUser =(data) =>{
     this.setState({user: {
@@ -108,7 +109,7 @@ displayFaceBox = (box) => {
 
   onRouteChange =(route)=>{
     if(route=== 'signout'){
-      this.setState({isSignedIn : false})
+      this.setState(initialState)
     }
     else if(route=== 'home'){
       this.setState({isSignedIn: true})
