3b19612ed1b883e0dd73b46d4b4ce4499b5dabbf
Haodong Zheng <a454400098@icloud.com>
2019-02-17 15:13:43 -0500

    update words on front page

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,11 +7,11 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
   return (
     <div>
       <p className ='f3'>
-        {'This Magic will detect faces in your pictures, Give it a try'}
+        {'Paste a Image URL with face phote, the magic will show up'}
       </p>
       <div  className ='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>  
+          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
           <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
             onClick = {onButtonSubmit}
